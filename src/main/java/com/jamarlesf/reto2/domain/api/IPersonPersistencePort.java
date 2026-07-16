efbff3a95776ea589cceca56cd456091e8f15229
Jorge Marles <jorge.marles@pragma.com.co>
2026-07-16 17:13:26 -0500

    fix(domain.api): delete unused import

--- src/main/java/com/jamarlesf/reto2/domain/api/IPersonPersistencePort.java
+++ src/main/java/com/jamarlesf/reto2/domain/api/IPersonPersistencePort.java
@@ -2,8 +2,6 @@ package com.jamarlesf.reto2.domain.api;
 
 import com.jamarlesf.reto2.domain.model.Person;
 
-import java.util.Optional;
-
 public interface IPersonPersistencePort {
     void save(Person person);
     Person findByDocumentId(String documentId);
